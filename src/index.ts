47e4b67a8665323e3b7379ab409a7bab7a7da1ee
Gabriel Trentini <gabriel.silveira1207@gmail.com>
2022-12-22 19:23:40 -0300

    operaçoes crud sem conexão com banco de dados

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
 import express, {Request, Response, NextFunction} from 'express';
+import usersRoute from './routes/users.route';
 
 const app = express();
 
+// Configurações da aplicação
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// Configurações de Rotas
+app.use(usersRoute);
+
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-    console.log('GET: /status')
+    console.log('GET: /status');
     res.status(200).send({foo: 'bar'});
 });
 
 
+// Inicialização do servidor
 app.listen(3000, () => {
-    console.log('Aplicação executando na porta 3000!')
+    console.log('Aplicação executando na porta 3000!');
 });
